e653799515e6411168c325da2a262125219e6b45
isnendyankp <isnendyankp@gmail.com>
2023-10-21 13:53:28 +0700

    ss7-224: Create ColorList component

---
+++ src/ColorList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ColorList = () => {
+  return (
+    <div>ColorList</div>
+  )
+}
+
+export default ColorList\
+
+// Progress:
+// - ss7-224: Create ColorList component
