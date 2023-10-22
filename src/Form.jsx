103f466cbfbadab85fe8d3e14db8c379f77b6fde
isnendyankp <isnendyankp@gmail.com>
2023-10-22 18:32:28 +0700

    Create handleSubmit function for form

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 const Form = () => {
   const [color, setColor] = useState('')
+  const handleSubmit = (e) => {
+
+  };
   return (
     <div>Form</div>
   )
@@ -12,3 +15,4 @@ export default Form
 // Progress:
 // - ss7-224: Create Form component
 // - ss7-224: Create color state
+// - ss7-224: Create handleSubmit function for form
