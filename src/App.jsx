00155b6fcec3fa64a1f6549de301d1271fd7d016
isnendyankp <isnendyankp@gmail.com>
2023-10-22 18:28:51 +0700

    Create main App with pass in Form and ColorList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  return (
+    <main>
+      <Form />
+      <ColorList />
+    </main>
+  );
 };
 export default App;
+
+// Progress:
+// - s7-224:Create main App with pass in Form and ColorList components
