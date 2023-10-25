2e98e7020e2d8c7b008e093deb3a63d0a436247f
isnendyankp <isnendyankp@gmail.com>
2023-10-25 11:08:53 +0700

    import Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ColorList from './ColorList';
+import Form from './Form';
 
 const App = () => {
   return (
@@ -13,3 +14,4 @@ export default App;
 // Progress:
 // - s7-224:Create main App with pass in Form and ColorList components
 // - s7-226:import ColorList component
+// - s7-226:import Form component
