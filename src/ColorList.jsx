0f951f3e61dea0753460432288ea706f1c6c60c8
isnendyankp <isnendyankp@gmail.com>
2023-10-26 11:02:53 +0700

    map over colors state

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const ColorList = ({ colors }) => {
   return (
     <section className='colors'>
+      {colors.map(() => {
 
+      })}
     </section>
   );
 };
@@ -14,3 +16,4 @@ export default ColorList
 // - s7-224: Create ColorList component
 // - s7-224: pass in colors state
 // - s7-225: Create colors section with className colors
+// - s7-225: map over colors state
