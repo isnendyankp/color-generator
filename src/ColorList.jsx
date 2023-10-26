5545dad17138b02ef65aecf44d686e6d0fb40f14
isnendyankp <isnendyankp@gmail.com>
2023-10-26 10:59:40 +0700

    pass in colors state

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const ColorList = () => {
-  return (
-    <div>ColorList</div>
-  )
-}
+const ColorList = ({ colors }) => {
+  return <div>ColorList</div>;
+};
 
 export default ColorList
 
 // Progress:
 // - s7-224: Create ColorList component
+// - s7-224: pass in colors state
