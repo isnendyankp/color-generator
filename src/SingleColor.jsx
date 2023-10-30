8ae1f81b6ef1d7370444445dada741769663ac04
isnendyankp <isnendyankp@gmail.com>
2023-10-30 08:52:37 +0700

    destructure hex and weight from color prop

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color
   return (
     <div>SingleColor</div>
   )
@@ -12,3 +13,4 @@ export default SingleColor
 // - s7-224: Create SingleColor component
 // - s7-227: pass in index prop
 // - s7-227: pass in color prop
+// - s7-227: destructure hex and weight from color prop
