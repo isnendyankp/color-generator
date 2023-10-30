a5c4f9a57bbe70956f5c8cc1871bc3aa28f18019
isnendyankp <isnendyankp@gmail.com>
2023-10-30 08:56:12 +0700

    add inline style with backgroundColor property

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const SingleColor = ({ index, color }) => {
   const { hex, weight } = color
   return (
-    <article className='color'>SingleColor</article>
+    <article 
+    className='color'
+    style={{ backgroundColor: `${hex}` }}
+    >
+      SingleColor
+    </article>
   )
 }
 
@@ -16,3 +21,4 @@ export default SingleColor
 // - s7-227: destructure hex and weight from color prop
 // - s7-227: Create article element with text SingleColor
 // - s7-227: add className color to article element
+// - s7-227: add inline style with backgroundColor property to article element
