690bbbc46f93a95f8d72d0d516d41915a1a6edbd
isnendyankp <isnendyankp@gmail.com>
2023-10-30 08:50:49 +0700

    pass in color prop

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SingleColor = ({ index }) => {
+const SingleColor = ({ index, color }) => {
   return (
     <div>SingleColor</div>
   )
@@ -11,3 +11,4 @@ export default SingleColor
 // progress:
 // - s7-224: Create SingleColor component
 // - s7-227: pass in index prop
+// - s7-227: pass in color prop
