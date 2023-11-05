27cadbcaad4b90ac5d84c91ba650eec71ccd1b3e
isnendyankp <isnendyankp@gmail.com>
2023-11-05 13:48:03 +0700

    check log color @addColor function

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
   const [colors, setColors] = useState(new Values('#f15025').all(10));
   
-  const addColor = (newColor) => {
-
+  const addColor = (color) => {
+    console.log(color)
   };
 
   return (
@@ -38,3 +38,4 @@ export default App;
 // - s7-228:add top-center position to ToastContainer
 // - s7-228:defind addColor function
 // - s7-228:pass in addColor function to Form component
+// - s7-228:check log color @addColor function
